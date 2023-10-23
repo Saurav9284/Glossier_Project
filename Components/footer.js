2e07d490f8bd9ff9406677f14bdff01905332b06
Raghavbhati <bhatiraghav4@gmail.com>
2023-10-23 15:26:04 +0530

    Footer DOne

---
+++ Components/footer.js
@@ -0,0 +1,57 @@
+function footer(){
+    return `
+    <div id="footerSection">
+        <div class="main_container">
+            <div id="footer_section1">
+                <div>
+                    <h4 class="footerTitles">Company</h4>
+                    <ul class="footerMenu">
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Stores</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Blog</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 class="footerTitles">Help</h4>
+                    <ul class="footerMenu">
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Shipping</a></li>
+                        <li><a href="#">Returns</a></li>
+                        <li><a href="#">Gift Card</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 id="footerLogo">Glossier</h2>
+                    <div id="socialbtnsDiv">
+                        <div id="footersocial">
+                            <a href="#"><i class="fa-brands fa-facebook" style="color: #ffffff;"></i></a>
+                            <a href="#"><i class="fa-brands fa-instagram" style="color: #ffffff;"></i></a>
+                            <a href="#"><i class="fa-brands fa-x-twitter" style="color: #ffffff;"></i></a>
+                            <a href="#"><i class="fa-brands fa-pinterest" style="color: #ffffff;"></i></a>
+                            <a href="#"><i class="fa-brands fa-linkedin" style="color: #ffffff;"></i></a>
+                        </div>
+                        <h3 id="bottomHeading">INTOTHEGLOSS</h3>
+                    </div>
+                </div>
+            </div>
+            <div id="copyrightFooter">
+                <div>
+                    <ul id="copyrightUl">
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms of Use</a></li>
+                        <li><a href="#">Accessibility</a></li>
+                        <li><a href="#">Supply Chain Transparency</a></li>
+                        <li><a href="#">Cookie Settings</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <p>© 2021 Glossier. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>`
+}
+<script src="https://kit.fontawesome.com/19da679b91.js" crossorigin="anonymous"></script>
+export {footer} ;
